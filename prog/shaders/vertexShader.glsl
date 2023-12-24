2845acbaecebdb4a703d2762667ea83a4fb72bbd
Connor Sweeney <connorsweeney1234@gmail.com>
2023-12-24 20:07:50 +0000

    better variable names

--- prog/shaders/vertexShader.glsl
+++ prog/shaders/vertexShader.glsl
@@ -3,11 +3,11 @@
 layout (location = 0) in vec3 position;
 layout (location = 1) in vec3 color;
 
-out vec3 v_vertexColor;
+out vec3 vVertexColor;
 
 void main()
 {
-    v_vertexColor = color;
+    vVertexColor = color;
 
     gl_Position = vec4(position.x, position.y, position.z, 1.0f);
 }
